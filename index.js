1edfd2e5f75adb9e3edfc4a4f39ef031b823d03b
TeaPot98 <trifautan@gmail.com>
2021-12-09 11:57:14 +0200

    update

--- index.js
+++ index.js
@@ -5,99 +5,99 @@ const cors = require('cors')
 
 const app = express()
 
-morgan.token('req-body', (request, response) => {
-    return JSON.stringify(request.body)
-})
+// morgan.token('req-body', (request, response) => {
+//     return JSON.stringify(request.body)
+// })
 
-app.use(cors())
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'))
+// app.use(cors())
+// app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'))
 
-const jsonParser = bodyParser.json()
+// const jsonParser = bodyParser.json()
 
-let persons = [
-    { 
-        "id": 1,
-        "name": "Arto Hellas", 
-        "number": "040-123456"
-    },
-    { 
-        "id": 2,
-        "name": "Ada Lovelace", 
-        "number": "39-44-5323523"
-    },
-    { 
-        "id": 3,
-        "name": "Dan Abramov", 
-        "number": "12-43-234345"
-    },
-    { 
-        "id": 4,
-        "name": "Mary Poppendieck", 
-        "number": "39-23-6423122"
-    }
-]
+// let persons = [
+//     { 
+//         "id": 1,
+//         "name": "Arto Hellas", 
+//         "number": "040-123456"
+//     },
+//     { 
+//         "id": 2,
+//         "name": "Ada Lovelace", 
+//         "number": "39-44-5323523"
+//     },
+//     { 
+//         "id": 3,
+//         "name": "Dan Abramov", 
+//         "number": "12-43-234345"
+//     },
+//     { 
+//         "id": 4,
+//         "name": "Mary Poppendieck", 
+//         "number": "39-23-6423122"
+//     }
+// ]
 
 
-app.get('/api/persons', (request, response) => {
-    response.json(persons)
-})
+// app.get('/api/persons', (request, response) => {
+//     response.json(persons)
+// })
 
-app.get('/info', (request, response) => {
-    const currentDate = new Date()
-    console.log(currentDate)
+// app.get('/info', (request, response) => {
+//     const currentDate = new Date()
+//     console.log(currentDate)
 
-    response.write(`<p>Phonebook has info for ${persons.length} people</p>`)
-    response.write(currentDate.toString())
-    response.end()
-})
+//     response.write(`<p>Phonebook has info for ${persons.length} people</p>`)
+//     response.write(currentDate.toString())
+//     response.end()
+// })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(p => p.id === id)
+// app.get('/api/persons/:id', (request, response) => {
+//     const id = Number(request.params.id)
+//     const person = persons.find(p => p.id === id)
 
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+//     if (person) {
+//         response.json(person)
+//     } else {
+//         response.status(404).end()
+//     }
     
-})
+// })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(p => p.id !== id)
+// app.delete('/api/persons/:id', (request, response) => {
+//     const id = Number(request.params.id)
+//     persons = persons.filter(p => p.id !== id)
 
-    response.status(204).end()
-})
+//     response.status(204).end()
+// })
 
-const generateId = () =>  Math.floor(Math.random() * 1000000)
+// const generateId = () =>  Math.floor(Math.random() * 1000000)
 
 
-app.post('/api/persons', jsonParser, (request, response) => {
-    const body = request.body
+// app.post('/api/persons', jsonParser, (request, response) => {
+//     const body = request.body
 
-    if (!body.name || !body.number) {
-        return response.status(400).json({
-            error: 'name or number missing'
-        })
-    }
+//     if (!body.name || !body.number) {
+//         return response.status(400).json({
+//             error: 'name or number missing'
+//         })
+//     }
     
-    if (persons.find(p => p.name === body.name)) {
-        return response.status(400).json({
-            error: 'name must be unique'
-        })
-    }
+//     if (persons.find(p => p.name === body.name)) {
+//         return response.status(400).json({
+//             error: 'name must be unique'
+//         })
+//     }
     
-    const person = {
-        id: generateId(),
-        name: body.name,
-        number: body.number
-    }
+//     const person = {
+//         id: generateId(),
+//         name: body.name,
+//         number: body.number
+//     }
     
-    persons = persons.concat(person)
+//     persons = persons.concat(person)
 
-    response.json(person)
-})
+//     response.json(person)
+// })
 
 
 const PORT = process.env.PORT || 3001
