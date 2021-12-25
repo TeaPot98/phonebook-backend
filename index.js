c36609addbd9591f37c2fcbff692691baf9dde6e
TeaPot98 <trifautan@gmail.com>
2021-12-25 23:01:46 +0200

    Finished part 3c

--- index.js
+++ index.js
@@ -22,28 +22,28 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :r
 
 const jsonParser = bodyParser.json()
 
-let persons = [
-    { 
-        "id": 1,
-        "name": "Arto Hellas", 
-        "number": "040-123456"
-    },
-    { 
-        "id": 2,
-        "name": "Ada Lovelace", 
-        "number": "39-44-5323523"
-    },
-    { 
-        "id": 3,
-        "name": "Dan Abramov", 
-        "number": "12-43-234345"
-    },
-    { 
-        "id": 4,
-        "name": "Mary Poppendieck", 
-        "number": "39-23-6423122"
-    }
-]
+// let persons = [
+//     { 
+//         "id": 1,
+//         "name": "Arto Hellas", 
+//         "number": "040-123456"
+//     },
+//     { 
+//         "id": 2,
+//         "name": "Ada Lovelace", 
+//         "number": "39-44-5323523"
+//     },
+//     { 
+//         "id": 3,
+//         "name": "Dan Abramov", 
+//         "number": "12-43-234345"
+//     },
+//     { 
+//         "id": 4,
+//         "name": "Mary Poppendieck", 
+//         "number": "39-23-6423122"
+//     }
+// ]
 
 
 app.get('/api/persons', (request, response) => {
@@ -55,10 +55,11 @@ app.get('/api/persons', (request, response) => {
 app.get('/info', (request, response) => {
     const currentDate = new Date()
     console.log(currentDate)
-
-    response.write(`<p>Phonebook has info for ${persons.length} people</p>`)
-    response.write(currentDate.toString())
-    response.end()
+    Person.find({}).then(persons => {
+        response.write(`<p>Phonebook has info for ${persons.length} people</p>`)
+        response.write(currentDate.toString())
+        response.end()
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
@@ -67,11 +68,17 @@ app.get('/api/persons/:id', (request, response) => {
     })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(p => p.id !== id)
+app.delete('/api/persons/:id', (request, response, next) => {
+    // const id = Number(request.params.id)
+    // persons = persons.filter(p => p.id !== id)
 
-    response.status(204).end()
+    // response.status(204).end()
+
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 const generateId = () =>  Math.floor(Math.random() * 1000000)
@@ -91,7 +98,7 @@ app.post('/api/persons', jsonParser, (request, response) => {
     //         error: 'name must be unique'
     //     })
     // }
-    
+
     const person = new Person({
         id: generateId(),
         name: body.name,
@@ -104,8 +111,37 @@ app.post('/api/persons', jsonParser, (request, response) => {
     })
 })
 
+app.put('/api/persons/:id', jsonParser, (request, response, next) => {
+    console.log('The PUT method is working')
+    const body = request.body
+
+    const person = {
+        id: request.params.id,
+        name: body.name,
+        number: body.number
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, {new: true})
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
