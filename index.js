1fb3ce04e448c5b421f11db1fb587fcdf7525c97
TeaPot98 <trifautan@gmail.com>
2021-12-26 21:11:05 +0200

    uibuild

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ app.put('/api/persons/:id', jsonParser, (request, response, next) => {
         number: body.number
     }
 
-    Person.findByIdAndUpdate(request.params.id, person, {new: true})
+    Person.findByIdAndUpdate(request.params.id, person, {new: true, runValidators: true})
         .then(updatedPerson => {
             response.json(updatedPerson)
         })
@@ -139,6 +139,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).send({error: error.message})
     }
 
     next(error)
